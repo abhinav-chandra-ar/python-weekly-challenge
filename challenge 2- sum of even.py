77b7fd952da402acdb032aec1f8d29304e9726eb
Abhinav Chandra A R <131592410+abhinav-chandra-ar@users.noreply.github.com>
2026-08-11 19:02:41 +0530

    Add input validation for sum of even numbers
    Added input validation to ensure the number is positive before calculating the sum of even numbers.

--- challenge 2- sum of even.py
+++ challenge 2- sum of even.py
@@ -1,8 +1,10 @@
 end = int(input("Enter a number : "))
 sum = 0
+if num > 0:
+    for num in range(end+1):
+        if num % 2 == 0:
+            sum += num
 
-for num in range(end+1):
-    if num % 2 == 0:
-        sum += num
-
-print("Sum of even numbers  : ", sum)
+    print("Sum of even numbers  : ", sum)
+else :
+    print("Enter a natural number!!")
